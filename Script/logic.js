908e3aabcc769dd91ddbd37747501a28a4bcf8ff
Aryan Ahmad Khan <134855998+AryanKhan10@users.noreply.github.com>
2023-08-27 18:26:56 +0500

    Update logic.js

--- Script/logic.js
+++ Script/logic.js
@@ -5,9 +5,9 @@
     const navBar = document.querySelector("#nav-list");
     console.log(manu);
     ham.addEventListener("click", () => {
-        ham.classList.toggle("change");                 //to convert 3 lines manu to cross(whose code is in css)
-        manu.classList.toggle("active");             //to display the manu list
-        search.classList.toggle("active");            //to display the search section
+        ham.classList.toggle("change");               
+        manu.classList.toggle("active");            
+        search.classList.toggle("active");            
         navBar.classList.toggle("active");
 
 
